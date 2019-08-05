05e7486f23f8d4d1547cb6fcb7b75e2eb48945f6
Leonel Bazan <leo.bazan.x@gmail.com>
2019-08-05 15:43:43 -0300

    tool-ui : creation of the basic skeleton of the ui

--- ui-sinatra/index.rb
+++ ui-sinatra/index.rb
@@ -1,5 +1,23 @@
 require 'sinatra'
 
+
+set :bind, '0.0.0.0'
+
 get '/' do
   erb :index
 end
+
+post '/answer'  do
+	erb :erb_answer
+end
+
+
+post '/answer'  do
+	erb :erb_answer
+end
+
+__END__
+
+
+@@ erb_answer
+"Hola"
