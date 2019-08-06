56931070079e7179da1cb45ca1d2c6aa3f396434
Leonel Bazan <leo.bazan.x@gmail.com>
2019-08-06 11:18:16 -0300

    tool-ui : the view can now show raw json data

--- ui-sinatra/index.rb
+++ ui-sinatra/index.rb
@@ -1,4 +1,7 @@
 require 'sinatra'
+require_relative '../response_json/filter_and_sort_functions_for_segments.rb'
+
+
 
 
 set :bind, '0.0.0.0'
@@ -12,12 +15,26 @@ post '/answer'  do
 end
 
 
-post '/answer'  do
+get '/answer'  do
 	erb :erb_answer
+
+
+
+  
+end
+
+post '/answer' do
+	redirect to('/answer')
 end
 
 __END__
 
-
 @@ erb_answer
-"Hola"
+-"Hola"
+
+<%=  object.password(@uuid, @challenge_code)%>
+
+data = JSON.parse(File.read('../response_json/flights.json'))
+  segments = get_segments(data)
+  segments = segments.take(10)
+  segments.each {|s| "#{s}\n"}
