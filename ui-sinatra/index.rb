f1dd005e5b974f6bf770b4ecf2cbeb36ab6f8255
Leonel Bazan <leo.bazan.x@gmail.com>
2019-08-07 15:07:10 -0300

    now the api can receive filters, apply them and return the filtered segments

--- ui-sinatra/index.rb
+++ ui-sinatra/index.rb
@@ -1,40 +1,21 @@
 require 'sinatra'
 require_relative '../response_json/filter_and_sort_functions_for_segments.rb'
 
-
-
-
 set :bind, '0.0.0.0'
 
+$data = JSON.parse(File.read('../response_json/flights.json'))
+$segments = get_segments($data)
+
+
 get '/' do
-  erb :index
+  content_type :json
+  json_received = JSON.parse(request.body.read)
+  filters = json_received["filters"]
+  sorts = json_received["sorts"]
+  segments = apply_filters($data, $segments, filters)
+  segments.to_json
 end
 
-post '/answer'  do
-	erb :erb_answer
-end
-
-
-get '/answer'  do
-	erb :erb_answer
-
-
-
-  
-end
-
-post '/answer' do
-	redirect to('/answer')
-end
 
 __END__
 
-@@ erb_answer
--"Hola"
-
-<%=  object.password(@uuid, @challenge_code)%>
-
-data = JSON.parse(File.read('../response_json/flights.json'))
-  segments = get_segments(data)
-  segments = segments.take(10)
-  segments.each {|s| "#{s}\n"}
