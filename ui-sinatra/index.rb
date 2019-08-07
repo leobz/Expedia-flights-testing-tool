66212514b693502846948765e6f76a094c77d366
Leonel Bazan <leo.bazan.x@gmail.com>
2019-08-07 16:01:13 -0300

    now the api can order the segments

--- ui-sinatra/index.rb
+++ ui-sinatra/index.rb
@@ -4,15 +4,16 @@ require_relative '../response_json/filter_and_sort_functions_for_segments.rb'
 set :bind, '0.0.0.0'
 
 $data = JSON.parse(File.read('../response_json/flights.json'))
-$segments = get_segments($data)
+$original_segments = get_segments($data)
 
 
 get '/' do
   content_type :json
   json_received = JSON.parse(request.body.read)
   filters = json_received["filters"]
-  sorts = json_received["sorts"]
-  segments = apply_filters($data, $segments, filters)
+  sort_type = json_received["sort_type"]
+  segments = apply_filters($data, $original_segments, filters)
+  segments = apply_sort(segments, sort_type)
   segments.to_json
 end
 
