5543a94f3e19dd111390e5b0fe7f0df5970a10b7
Leonel Bazan <leo.bazan.x@gmail.com>
2019-08-21 15:52:24 -0300

    API-REST : Some variable names were changed For more expressiveness

--- back-end/api_rest/index.rb
+++ back-end/api_rest/index.rb
@@ -1,24 +1,26 @@
 require 'sinatra'
 require_relative '../response_json/filter_and_sort_functions_for_segments.rb'
 
-def generate_response(data, segments)
+def generate_response(json_received, segments)
+  flights_data = json_received["flightsData"]['payload']
   response = {"flightCards" => segments, 
               "availableFlightNumbers" => get_flight_numbers(segments), 
               "availablePrices" => get_prices(segments), 
               "availableAirlines" => get_airline_names(segments),
-              "availableStops" => get_stops_amounts(data, segments),
-              "itinerariesSize" => itineraries_size(data),
+              "availableStops" => get_stops_amounts(flights_data, segments),
+              "itinerariesSize" => itineraries_size(flights_data),
             }
   return response
 end
 
-def process_segments(data, segments_id, filters, sort_type)
-  segments = get_segments(data, segments_id)
-  segments = apply_filters(data, segments, filters)
+def process_segments(flights_data, segments_id, filters, sort_type)
+  segments = get_segments(flights_data, segments_id)
+  segments = apply_filters(flights_data, segments, filters)
   return apply_sort(segments, sort_type)
 end
 
-def process_flights_data(flights_data, json_received)
+def process_flights_data(json_received)
+  flights_data = json_received["flightsData"]['payload']
   filters = json_received["filters"]
   sort_type = json_received["sortType"]
   segments_id = json_received["segmentsId"].nil? ? [] : json_received["segmentsId"]
@@ -31,9 +33,8 @@ post '/ui_test' do
   content_type :json
 
   json_received = JSON.parse(request.body.read)
-  flights_data = json_received["flightsData"]['payload']
-  segments = process_flights_data(flights_data, json_received)
-  return generate_response(flights_data, segments).to_json
+  segments = process_flights_data(json_received)
+  return generate_response(json_received, segments).to_json
 end
 
 __END__
