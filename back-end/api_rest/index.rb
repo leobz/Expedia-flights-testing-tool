b7101e7f78fee49fe9e47f20b2360fb2145e5778
Leonel Bazan <leo.bazan.x@gmail.com>
2019-08-21 15:19:41 -0300

    Little refactor in API-REST
    Some repeated logic was eliminated

--- back-end/api_rest/index.rb
+++ back-end/api_rest/index.rb
@@ -15,8 +15,14 @@ end
 def process_segments(data, segments_id, filters, sort_type)
   segments = get_segments(data, segments_id)
   segments = apply_filters(data, segments, filters)
-  segments = apply_sort(segments, sort_type)
-  return segments
+  return apply_sort(segments, sort_type)
+end
+
+def process_flights_data(flights_data, json_received)
+  filters = json_received["filters"]
+  sort_type = json_received["sortType"]
+  segments_id = json_received["segmentsId"].nil? ? [] : json_received["segmentsId"]
+  return process_segments(flights_data, segments_id, filters, sort_type)
 end
 
 set :bind, '0.0.0.0'
@@ -25,52 +31,39 @@ post '/ui_test' do
   #This is the EndPoint that Receives all the flights' itineraries data from the UI and
   #not for one hard coded JSON
   content_type :json
+
   json_received = JSON.parse(request.body.read)
   flights_data = json_received["flightsData"]['payload']
-  filters = json_received["filters"]
-  sort_type = json_received["sortType"]
-  segments_id = json_received["segmentsId"].nil? ? [] : json_received["segmentsId"]
-
-  segments = process_segments(flights_data, segments_id, filters, sort_type)
-  generate_response(flights_data, segments).to_json
+  segments = process_flights_data(flights_data, json_received)
+  return generate_response(flights_data, segments).to_json
 end
 
 
 #HARDCODED EndPoints
 post '/flights' do
   content_type :json
-  data = JSON.parse(File.read('../response_json/flights.json'))
-  json_received = JSON.parse(request.body.read)
-  filters = json_received["filters"]
-  sort_type = json_received["sort_type"]
-  segments_id = json_received["segments_id"].nil? ? [] : json_received["segments_id"]
 
-  segments = process_segments(data, segments_id, filters, sort_type)
-  generate_response(data, segments).to_json
+  flights_data = JSON.parse(File.read('../response_json/flights.json'))
+  json_received = JSON.parse(request.body.read)
+  segments = process_flights_data(flights_data, json_received)
+  generate_response(flights_data, segments).to_json
 end
 
 post '/multicity_4_cities' do
   content_type :json
-  data_multicity_4_cities = JSON.parse(File.read('../response_json/multicity_4_cities.json'))['payload']
+  flights_data = JSON.parse(File.read('../response_json/multicity_4_cities.json'))['payload']
   json_received = JSON.parse(request.body.read)
-  filters = json_received["filters"]
-  sort_type = json_received["sort_type"]
-  segments_id = json_received["segments_id"].nil? ? [] : json_received["segments_id"]
-
-  segments = process_segments(data_multicity_4_cities, segments_id, filters, sort_type)
-  generate_response(data_multicity_4_cities, segments).to_json
+  segments = process_flights_data(flights_data, json_received)
+  generate_response(flights_data, segments).to_json
 end
 
 post '/roundtrip' do
   content_type :json
-  data_roundtrip = JSON.parse(File.read('../response_json/roundtrip.json'))['payload']
-  json_received = JSON.parse(request.body.read)
-  filters = json_received["filters"]
-  sort_type = json_received["sort_type"]
-  segments_id = json_received["segments_id"].nil? ? [] : json_received["segments_id"]
 
-  segments = process_segments(data_roundtrip, segments_id, filters, sort_type)
-  generate_response(data_roundtrip, segments).to_json
+  flights_data = JSON.parse(File.read('../response_json/roundtrip.json'))['payload']
+  json_received = JSON.parse(request.body.read)
+  segments = process_flights_data(flights_data, json_received)
+  generate_response(flights_data, segments).to_json
 end
 
 __END__
